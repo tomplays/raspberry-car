57d495f6bd47a6f04c6bba54bcc6386e72fe0609
tomplays <homeof@gmail.com>
2014-01-22 12:43:55 +0100

    doc+

--- run.py
+++ run.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
 
 # Raspberry Car v1.xx
-#https://github.com/tomplays/raspberry-car
-# GPL 2014 - Tom Wersinger 
+# MIT Licence - Tom Wersinger https://github.com/tomplays/raspberry-car/LICENCE.md
 
 import RPi.GPIO as io
 import time
 
-io.setmode(io.BCM)
+
+# pins attribution
+#motor A
 in3_pin = 4
 in4_pin = 17
+
+#motor B
 in1_pin = 27
 in2_pin = 22
-glowtime = .1
-turntime = .04
+
+#Blinking orange leds
 o_pin = 24
 or_pin = 23
 
 # always stop motors after xx seconds..
 securetime = 10
 
+#misc def
+glowtime = .1
+turntime = .04
+
+#GPIO inits
+io.setmode(io.BCM)
 io.setup(in1_pin, io.OUT)
 io.setup(in2_pin, io.OUT)
 io.setup(in3_pin, io.OUT)
@@ -27,19 +36,19 @@ io.setup(in4_pin, io.OUT)
 io.setup(o_pin, io.OUT)
 io.setup(or_pin, io.OUT)
 
+#demo mode for glow
 def demoa():
     # alone left and right
     glow(False, True, .2, 5, "yes")
     glow(True, False, .2, 5, "yes")
 
-    # both together and together alternate
+    # both together and both alternate
     glow(True, True, .1, 5, "yes")
     glow(True, True, .1, 5, "no")
 
     stopall()
 
-
-
+#make the two orange leds blink
 def glow(a,b,tim, ran, alternate):
     for x in range(0, ran):
         if alternate == "yes":
@@ -68,7 +77,8 @@ def glow(a,b,tim, ran, alternate):
     io.output(o_pin, False)
     io.output(or_pin, False)
 
-
+#params
+#   dir: left|right
 def turn(dir):
     if dir == "right":
         io.output(in3_pin, False)
@@ -84,9 +94,11 @@ def turn(dir):
     io.output(in4_pin, False)
 
 
-
+#params
+#   dir: forward|backward
+#   long: how long (to check)
 def drive(dir, long):
-    if dir == "ahead":
+    if dir == "forward":
         io.output(in1_pin, False)
         io.output(in2_pin, True)
     if dir == "back":
@@ -96,7 +108,7 @@ def drive(dir, long):
     io.output(in1_pin, False)
     io.output(in2_pin, False)
 
-
+# turns all gpio off
 def stopall():
     io.output(in1_pin, False)
     io.output(in2_pin, False)
@@ -105,6 +117,9 @@ def stopall():
     io.output(o_pin, False)
     io.output(or_pin, False)
 
+# Main loop
+# expects keyboard inputs:
+# zw(drive) - ae(turn) - lm -hg(glows) - s(stop) p(demo) :
 while True:
     cmd = raw_input("zw - ae - lm - hg - s :")
     direction = cmd[0]
@@ -115,7 +130,7 @@ while True:
     elif direction == "s":
         stopall()
     elif direction == "z":
-        drive('ahead',1)
+        drive('forward',1)
     elif direction == "w":
         drive('back', 1)
     elif direction == "p":
